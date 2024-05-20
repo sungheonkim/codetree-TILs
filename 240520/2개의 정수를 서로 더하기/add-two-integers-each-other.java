f2426ce124b304973aaa86304b542c9c8f82a440
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-05-20 23:42:30 +0900

    Title: 2개의 정수를 서로 더하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/2개의 정수를 서로 더하기/add-two-integers-each-other.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a+=b;
+        System.out.printf("%d ",a);
+        b+=a;
+        System.out.printf("%d",b);
+       
+       
+       
+    }
+}
