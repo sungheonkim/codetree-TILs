5bf8a985c00b663e09b27c1c76533727e6dadcff
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-05-23 22:17:09 +0900

    Title: 등장횟수 확인, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240523/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        
+        int cnt1=0;
+        for(int i=1;i<=5;i++)
+        {
+            int n=sc.nextInt();
+            if(n%2==0)
+            {
+                cnt1++;
+            }
+    
+        }
+        System.out.printf("%d",cnt1);
+
+    }
+}
