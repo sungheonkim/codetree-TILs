88a39abb0fc8ad5519d5e76302b2fe06a04f15f5
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-05-25 13:33:00 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240525/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<10;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
