c9b26a5115266501ab9613d8541e861e6c9d3e52
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-05-25 15:38:32 +0900

    Title: 함수를 이용한 윤년 판별, Time: 179ms, Memory: 10MB, Status: Passed - CodeTree

--- 240525/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 240525/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -5,7 +5,7 @@ public class Main {
     {
         if(n%4==0)
         {
-            if(n%100==0&&n%400==0)
+            if(n%100==0&&n%400!=0)
             {
                 return false;
             }
