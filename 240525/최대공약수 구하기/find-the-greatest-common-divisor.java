31ad829c2bdac9d09b0eff46026629e306be8407
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-05-25 13:51:11 +0900

    Title: 최대공약수 구하기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 240525/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 240525/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -1,34 +1,25 @@
 import java.util.Scanner;
 public class Main {
+    public static void gcd(int a,int b)
+    {
+       int max=0;
+       for(int i=1;i<=Math.min(a,b);i++)
+       {
+            if(a%i==0&&b%i==0)
+            {
+                max=i;
+            }
+       }
+       System.out.print(max);
+
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        int max=0;
-       
-       if(a>b)
-       {
-        for(int i=1;i<a;i++)
-        {
-            if(a%i==0&&b%i==0)
-            {
-                max=i;
-            }
-        }
-       }
-       else if(a<b)
-       {
-            for(int i=1;i<b;i++)
-        {
-            if(a%i==0&&b%i==0)
-            {
-                max=i;
-            }
-        }
-       }
-       else{
-        max=a;
-       }
-       System.out.print(max);
+        
+      
+        gcd(a,b);
     }
 }
