2150a82be1a496cc14d7237b42415112adeabb3a
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-05-28 22:40:18 +0900

    Title: 단어로 구분하기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/단어로 구분하기/separate-words-with-words.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    
+        String[] arr = new String[10];
+        
+      
+        int cnt=0;
+        int sum=0;
+        for(int i=0;i<10;i++)
+            {
+                arr[i]=sc.next();
+               
+            }   
+        for(int i=0;i<10;i++)
+            {
+               System.out.println(arr[i]); 
+            }   
+          
+    }
+}
