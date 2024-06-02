47a50548e962d73385ad01580e66ccb0c6160650
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-06-02 23:02:42 +0900

    Title: 문자열 추가하기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240602/문자열 추가하기/add-spring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str =sc.next();
+      
+       str=str+"Hello";
+
+       System.out.print(str);
+}
+}
