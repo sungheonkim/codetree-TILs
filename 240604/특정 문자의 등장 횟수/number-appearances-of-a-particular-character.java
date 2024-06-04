5d231f66b84bbe2f4bd02da6005aceec29a65e63
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-06-04 10:32:18 +0900

    Title: 특정 문자의 등장 횟수, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        String str=sc.next();
+        int cnt1=0,cnt2=0;
+        for(int i=0;i<str.length()-1;i++)
+        {
+        if(str.substring(i,i+2).equals("ee"))
+        {
+           cnt1++;
+        }
+        if(str.substring(i,i+2).equals("eb"))
+        {
+           cnt2++;
+        }
+    }
+    System.out.print(cnt1+" "+cnt2);
+       
+    }
+}
