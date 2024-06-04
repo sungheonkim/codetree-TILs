3e39433b9772ace2e00fdc228e3d4752cee31379
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-06-04 09:50:51 +0900

    Title: 특정 문자의 유무, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240604/특정 문자의 유무/specific-character-presence.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc= new Scanner(System.in);
+        String str=sc.next();
+        if(str.contains("ee"))
+        {
+            System.out.print("Yes ");
+        }
+        else
+        {
+            System.out.print("No ");
+        }
+
+         if(str.contains("ab"))
+        {
+            System.out.print("Yes ");
+        }
+        else
+        {
+            System.out.print("No ");
+        }
+    }
+}
