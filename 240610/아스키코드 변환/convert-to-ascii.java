b6f359a373ae460ebda9b87dd157f1d92d64fa86
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-06-10 21:42:29 +0900

    Title: 아스키코드 변환, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        int a=(int)sc.next().charAt(0);
+        char b=(char)sc.nextInt();
+
+        System.out.print(a+" "+b);
+    }
+}
