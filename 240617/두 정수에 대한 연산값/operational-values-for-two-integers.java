bdf144db9c79a9bbd91efc0a19c449a4335a8e39
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-06-17 19:39:07 +0900

    Title: 두 정수에 대한 연산값, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240617/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static int callMax(int a,int b)
+    {
+        return Math.max(a,b)+25;
+    }
+    public static int callMin(int a,int b)
+    {
+        return Math.min(a,b)*2;
+    }
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+
+        System.out.print(callMin(a,b)+" "+callMax(a,b));
+
+
+    }
+}
