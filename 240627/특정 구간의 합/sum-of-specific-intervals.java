4766c64e14e9e123521200954d57c65fe5f3f2e9
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2024-06-27 10:57:35 +0900

    Title: 특정 구간의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240627/특정 구간의 합/sum-of-specific-intervals.java
+++ 240627/특정 구간의 합/sum-of-specific-intervals.java
@@ -1,45 +1,44 @@
 import java.util.*;
 
-class Sum{
+
+public class Main {
     public static int[] arr;
 
     //생성시 input도 받기
-    public static void Sum(int a){
+    public static void Input(int a){
         arr=new int[a];
-        Scanner sc=new Scanner(System.in);
+        System.out.println(a);
+     
 
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
+
     }
 
-    public static void result(int i,int j){
+    public static void Result(int i,int j){
         int sum=0;
         for(int k=i-1;k<j;k++){
             sum+=arr[k];
-        }   
+        }
         System.out.println(sum);
     }
-    
-}
-public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc=new Scanner(System.in);
 
         int a=sc.nextInt();
         int b=sc.nextInt();
+        System.out.print(a+" "+b);
+        Input(a);// 배열 입력받기
 
-        Sum(a);// 배열 입력받기
-
-        for(int i=0;i<b;i++){
-            
+        for(int k=0;k<b;k++){
             int i=sc.nextInt();
             int j=sc.nextInt();
-            result(i,j);
+            Result(i,j);
         }
+        sc.close();
 
-        
-        
+
     }
 }
