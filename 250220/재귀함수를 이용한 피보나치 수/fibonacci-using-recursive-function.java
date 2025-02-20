fa2db03dff7da853b118c2d4726e395fd7995fc1
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-02-20 16:04:29 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250220/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(F(n));
+
+    }
+    public static int F(int n){
+        if(n==1) return 1;
+        if(n==2) return 1;
+
+        return F(n-2)+F(n-1);
+    }
+}
