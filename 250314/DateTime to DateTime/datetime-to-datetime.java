fff3ff42a7d862ae66e7c9a69c21ee57c0826bf3
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-03-14 11:42:09 +0900

    Title: DateTime to DateTime, Time: 3011ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 250314/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        // Please write your code here.
+        int currA=11;
+        int currB=11;
+        int currC=11;
+        int total=0;
+        while(true){
+            if((A==currA)&&(B==currB)&&(C==currC)) break;
+            total++;//시간증가
+            currC++; 
+            if(currC==60){
+                currC=0;
+                currB++;
+                if(currB==24){
+                    currB=0;
+                    currA++;
+                }
+            }
+        }
+        System.out.println(total);
+    }
+}
