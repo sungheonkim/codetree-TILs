c175a0bec68e148bd6609840feab03343232f51d
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-03-14 12:03:07 +0900

    Title: 그 요일은, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250314/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A=sc.next();
+       
+        String[] result=new String[]{"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+       
+        int diff=numOfDays(m2,d2)-numOfDays(m1,d1);
+
+        while(diff<0)
+            diff+=7;
+
+        System.out.print(diff % 7);
+    }
+    public static int numOfDays(int m, int d) {
+        // 계산 편의를 위해 월마다 몇 일이 있는지를 적어줍니다. 
+        int[] days = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int totalDays = 0;
+        
+        // 1월부터 (m - 1)월 까지는 전부 꽉 채워져 있습니다.
+        for(int i = 1; i < m; i++)
+            totalDays += days[i];
+        
+        // m월의 경우에는 정확이 d일만 있습니다.
+        totalDays += d;
+        
+        return totalDays;
+    }
+}
