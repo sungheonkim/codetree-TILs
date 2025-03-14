a42f95ba7fb7e5e77d3ff1ba07ec79d3e0d33311
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-03-14 11:45:10 +0900

    Title: DateTime to DateTime, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 250314/DateTime to DateTime/datetime-to-datetime.java
+++ 250314/DateTime to DateTime/datetime-to-datetime.java
@@ -1,34 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        // Please write your code here.
-        int currA=11;
-        int currB=11;
-        int currC=11;
-        int total=0;
-
-       
-        if((B<11)||(B==11&&C<11)){ 
-            System.out.println(-1);
-            return;
-        }
-        while(true){
-            if((A==currA)&&(B==currB)&&(C==currC)) break;
-            total++;//시간증가
-            currC++; 
-            if(currC==60){
-                currC=0;
-                currB++;
-                if(currB==24){
-                    currB=0;
-                    currA++;
-                }
-            }
-        }
-        System.out.println(total);
+        // 변수 선언 및 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        // 차이를 계산합니다.
+        int diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11);
+        
+        // 출력
+        if(diff < 0)
+            System.out.print(-1);
+        else
+            System.out.print(diff);
     }
 }
