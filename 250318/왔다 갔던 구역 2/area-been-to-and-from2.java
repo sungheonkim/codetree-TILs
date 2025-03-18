405536c182458fb0ac10bf2bf608e727e1e23c1d
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-03-18 12:04:36 +0900

    Title: 왔다 갔던 구역 2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250318/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Main {
+    public static int curr=0; // 현재 좌표
+    public static int[] visit; // 구간 체크하는 용도
+    public static int OFFSET=100;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        visit=new int[OFFSET*2+1];
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char dir = sc.next().charAt(0);
+            
+
+            if(dir=='R') right(x);
+            if(dir=='L') left(x);
+            // Please write your code here.
+        }
+        int cnt=0;
+        for(int i=0;i<OFFSET*2+1;i++){
+            if(visit[i]>1) cnt++;
+        }
+        System.out.print(cnt);
+    }
+    //현재 좌표부터 x만큼 양수방향으로 이동하면서 지나간 공간에 ++
+    public static void right(int x){
+        for(int i=curr;i<curr+x;i++){
+            visit[i+OFFSET]++;
+        }
+        //마지막으로 좌표 수정해주기
+        curr+=x;
+
+    }
+    //현재 좌표부터 x만큼 음수방향으로 이동하면서 지나간 공간에 ++
+    public static void left(int x){
+        for(int i=curr;i>curr-x;i--){
+            visit[i+OFFSET]++;
+        }
+        //마지막으로 좌표 수정해주기
+        curr-=x;
+    }
+}
