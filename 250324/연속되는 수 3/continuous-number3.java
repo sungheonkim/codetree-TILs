be069ed525c1bd3893e71c5e9ebc19ca1ca4b37b
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-03-24 11:12:54 +0900

    Title: 연속되는 수 3, Time: 183ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250324/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        int cnt=0,max=0;
+        //둘이 곱했을 때 양수면 부호가 같은거지ㅋ
+        for(int i=0;i<n;i++){
+            if(i>=1&&arr[i]*arr[i-1]>0){
+                cnt++;
+            }
+            else cnt=1;
+            max=Math.max(cnt,max);
+            //System.out.println(arr[i]+" cnt: "+cnt);
+        }
+        
+        System.out.println(max);
+        // Please write your code here.
+    }
+}
