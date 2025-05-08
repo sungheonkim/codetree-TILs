3d5ea24ae0f8379d1be2f5b6154962589a7b4423
sungheonkim <143686086+sungheonkim@users.noreply.github.com>
2025-05-08 12:37:23 +0900

    Title: 되돌아오기 2, Time: 148ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250508/되돌아오기 2/come-back-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+
+    //시계 방향으로 돌 수 있도록 해준다
+    public static int[] dx= new int[]{1,0,-1,0};
+    public static int[] dy= new int[]{0,-1,0,1};
+    public static int x,y=0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String commands = sc.next();
+        int len= commands.length();
+
+        //북쪽 방향 바라보고 시작
+        int dir=3;
+        int time=0;
+
+        for(int i=0;i<len;i++){
+            char cDir=commands.charAt(i);
+
+            if(cDir=='L') dir=(dir-1+4)%4;
+            else if(cDir=='R') dir=(dir+1)%4;
+            else{
+                x+=dx[dir];
+                y+=dy[dir];
+            }
+            time++;
+
+            if(x==0&&y==0) break;
+
+        }
+        System.out.println(time);
+        // Please write your code here.
+    }
+}
